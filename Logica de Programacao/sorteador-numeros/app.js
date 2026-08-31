6805a5b9d7dec6a204d629539c5e4df312b62779
Caio Reischoffer <caioreischoffer@gmail.com>
2026-08-31 16:57:31 -0300

    Aula 1 - Sorteador de números | Concluida + Protecao.

--- Logica de Programacao/sorteador-numeros/app.js
+++ Logica de Programacao/sorteador-numeros/app.js
@@ -1,19 +1,30 @@
-function sortear() {
-    let quantidade = parseInt(document.getElementById("quantidade").value);
-    let de = parseInt(document.getElementById("de").value);
-    let ate = parseInt(document.getElementById("ate").value);
+function sortear(){
+  let quantidade = parseInt(document.getElementById('quantidade').value);
+  let de = parseInt(document.getElementById('de').value);
+  let ate = parseInt(document.getElementById('ate').value);
 
+      if (de >= ate) {
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+        return;
+        }
+     
+      if (quantidade > (ate - de + 1)) {
+    alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
+    return;
+  }
    let sorteados = [];
    let numero;
 
     for (let i = 0; i < quantidade; i++) {
-       numero = obterNumeroAleatorio(de, ate);
+    numero = obterNumeroAleatorio(de, ate);
 
-      while (sorteados.includes(numero)) {
-          numero = obterNumeroAleatorio(de, ate);
-       }
+    while (sorteados.includes(numero)) {
+      numero = obterNumeroAleatorio(de, ate);
+      alert('Tentando obter número inédito');
+    }
+
     sorteados.push(numero);
-   }
+  }
 
    let resultado = document.getElementById("resultado");
    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
